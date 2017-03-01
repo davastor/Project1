fb1fb4bba68b556fd15d07cee5730caf863bc491
Dan Dang <Dan Dang>
2017-02-28 21:27:44 -0800

    Added functions to main.cpp

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -3,23 +3,27 @@
 #include "Monster.h"
 #include "Character.h"
 //#include "Items.h"
-//#include "Abilities.h"
+#include "Abilities.h"
 
 using namespace std;
 
-Character CharacterSet();
-void Rounds(Character player);
-void Combat(Monster monster, Character player);
+Character CharacterCreation();
+void Rounds(Character player, Abilities ability);
+void Combat(Monster monster, Character player, Abilities ability);
+void Ability(Character character, Abilities ability);
 
 int main()
 {
-	Rounds(CharacterSet());
+	Character character = CharacterCreation();
+	Abilities ability = Abilities(character.getClass());
+
+	Rounds(character, ability);
 
 	system("PAUSE");
 	return 0;
 }
 
-Character CharacterSet()
+Character CharacterCreation()
 {
 	string name;
 	char prof;
@@ -35,35 +39,55 @@ Character CharacterSet()
 	return player;
 }
 
-void Rounds(Character player)
+void Rounds(Character character, Abilities ability)
 {
 	for (int round = 0; round < 5; round++)
 	{
 		Monster monster = Monster(round);
 		cout << "A " + monster.getMonsterName() + " appears!" << endl;
-		Combat(monster, player);
+		Combat(monster, character, ability);
 	}
 }
 
-void Combat(Monster monster, Character player)
+void Combat(Monster monster, Character character, Abilities ability)
 {
 	
 	char action;
 	while (monster.getMonsterHealth() >= 0)
 	{
 		cout << "Monster: " + monster.getMonsterName();
-		cout << "Health: " + monster.getMonsterAttack();
+		cout << "Health: " + monster.getMonsterHealth();
 		cout << "Attack: " + monster.getMonsterAttack();
 
 		cout << "What will you do? Use a (M)elee attack?  Or use an (A)bility?" << endl;
 		cin >> action;
+
 		if (toupper(action) == 'M')
 		{
-			monster.subtractDamage(player.getAttack());
+			monster.subtractDamage(character.getAttack());
+			cout << "You deal " << character.getAttack() << " damage.";
+
 		}
 		else
 		{
-
+			Ability(character, ability);
 		}
+
+		if (monster.getMonsterHealth() >= 0)
+		{
+			cout << "The " + monster.getMonsterName() + " hits back for " 
+				 << monster.getMonsterAttack() << " damage." << endl;
+		}
+	}
+
+	cout << "You have defeated the" + monster.getMonsterHealth() << endl;
+}
+
+void Ability()
+{
+	cout << "What ability will you use?" << endl;
+	if (player.getClass*( == "Warrior")
+	{
+		cout << ""
 	}
 }
